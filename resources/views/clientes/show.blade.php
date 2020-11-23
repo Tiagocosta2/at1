15c4be3c05091804bf3f376e52bc576b223baf8d
TIAGO <TIAGO@ASUS.local>
2020-11-23 17:07:02 +0000

    completo_bootstrap

--- resources/views/clientes/show.blade.php
+++ resources/views/clientes/show.blade.php
@@ -1,19 +1,44 @@
+@extends('layout')
+@section('conteudo')
+<div class="w-100 p-3" style="background-color: #eee;">
+<table class="table table-dark" >
+	<thead>
+		<tr>
+			@if(isset($cliente->id_cliente))
+			<th scope="col" ><h1> Detalhes do Cliente {{$cliente->id_cliente}}</h1></th>
+		</tr>
+</thead>
+<tbody>
+	<tr>
+		<th scope="row">Nome</th>
+      	<td>{{$cliente->nome}}</td>
+	</tr>
+	<tr>
+		<th scope="row">Morada</th>
+      	<td>{{$cliente->morada}}</td>
+	</tr>
+	<tr>
+		<th scope="row">Telefone</th>
+      	<td>{{$cliente->telefone}}</td>
+	</tr>
+	<tr>
+		<th scope="row">Email</th>
+      	<td>{{$cliente->email}}</td>
+	</tr>
 
-@if(isset($cliente->id_cliente))
-<h1> Detalhes do Cliente {{$cliente->id_cliente}}</h1>
-
-Nome:{{$cliente->nome}}<br>
-Morada:{{$cliente->morada}}<br>
-Telefone:{{$cliente->telefone}}<br>
-Email:{{$cliente->email}}<br>
 
 @else
 <h1 style="color:#ff0000">Erro</h1>
 
 @endif
 
-
-Encomenda:
-@foreach($cliente->encomendas as $encomenda)
-{{$encomenda->data}}
-@endforeach
+<tr>
+	<th scope="row">Encomenda</th>
+	@foreach($cliente->encomendas as $encomenda)
+      <td>{{$encomenda->data}}</td>
+	@endforeach
+</tr>
+</tbody>
+</table>
+</div>
+@endsection 
--- resources/views/produtos/show.blade.php
+++ resources/views/produtos/show.blade.php
@@ -1,13 +1,38 @@
-@if(isset($produto->id_produto))
-<h1> Detalhes do Produto {{$produto->id_produto}}</h1>
-Designação:{{$produto->designacao}}<br>
-Stock:{{$produto->stock}}<br>
-Preço:{{$produto->preco}} €<br>
+@extends('layout')
+@section('conteudo')
+<div class="w-100 p-3" style="background-color: #eee;">
+<table class="table table-dark">
+	<thead>
+		<tr>
+		@if(isset($produto->id_produto))
+		<th scope="col"><h1> Detalhes do Produto {{$produto->id_produto}}</h1></th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+		<th scope="row">Designação</th>
+      	<td>{{$produto->designacao}}</td>
+		</tr>
+		<tr>
+		<th scope="row">Stock</th>
+      	<td>{{$produto->stock}}</td>
+		</tr>
+		<tr>
+		<th scope="row">Preço</th>
+      	<td>{{$produto->preco}}€</td>
+	</tr>
 
 @else
 <h1 style="color:#ff0000">Erro</h1>
 @endif
-Encomenda:
-@foreach($produto->encomendas as $encomenda)
-{{$encomenda->data}}<br>
+<tr>
+
+	<th scope="row">Encomenda</th>
+	@foreach($produto->encomendas as $encomenda)
+    <td><a href="{{route('encomendas.show', ['id'=>$encomenda->id_encomenda])}}">{{$encomenda->data}}</a></td>
 @endforeach
+</tr>
+	</tbody>
+</table>
+</div>
+@endsection
